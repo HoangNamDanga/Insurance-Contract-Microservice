ebc4bd3a03b40df859fd39bf52ef1be41146b513
HoangNamDanga <152879195+HoangNamDanga@users.noreply.github.com>
2025-12-31 14:02:30 +0700

    Chinh thuc trien khai CI/CD va xu ly Unacked

---
+++ Insurance.Tests/PolicyChangedConsumerTests.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using MongoDBCore.Interfaces;
+using MongoDBCore.Repositories.Consumer;
+using Shared.Contracts.Events;
+using System.Net.WebSockets;
+
+namespace Insurance.Tests
+{
+    public class PolicyChangedConsumerTests
+    {
+        [Fact]
+        public async Task Consume_CancelAction_ShouldCallUpdateCancelStatus()
+        {
+            // 1. Arrange: Chuẩn bị giả lập
+            var mocRepo = new Mock<IPolicyRepository>();
+            var consumer = new PolicyChangedConsumer(mocRepo.Object);
+            var mockContext = new Mock<ConsumeContext<PolicyChangedEvent>>();
+
+            var message = new PolicyChangedEvent
+            {
+                ActionType = "CANCEL",
+                PolicyId = 101,
+                LastNotes = "Test CI/CD"
+            };
+
+            mockContext.Setup(x => x.Message).Returns(message);
+
+            // 2. Act: Chay thu ham Consume
+            await consumer.Consume(mockContext.Object);
+
+            //3. Assert: Kiem tra xem ham UpdateCanelStatus co duoc goi dung 1 lan
+            mocRepo.Verify(x => x.UpdateCancelStatusAsync(101, "Test CI/CD"), Times.Once);
+        }   
+    }
+}
